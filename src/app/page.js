15c38bffa5f9b429f7bd97504e2b0e0f8a465cb6
milenaggoes <rm94305@fiap.com.br>
2023-09-04 10:02:49 -0300

    add img

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,78 @@
-import NavBar from "@/components/NavBar"
+import NavBar from "@/components/NavBar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
+      <main className="px-20 py-14">
+        <h1 className="text-center mb-10">Selecione um de nossos quartos disponíveis</h1>
+        <div className="">
+          <div className="flex justify-evenly flex-wrap cursor-pointer">
+            <div className="mb-10">
+              <Image
+                src="/premium.png"
+                width={300}
+                height={146}
+                alt="Picture of the author"
+              />
+              <h2>Quarto de luxo</h2>
+              <h2>R$500 diária</h2>
+            </div>
+            <div>
+              <Image
+                src="/doissolteiro.png"
+                width={300}
+                height={146}
+                alt="Picture of the author"
+              />
+              <h2>Quarto duplo solteiro</h2>
+              <h2>R$350 diária</h2>
+            </div>
+            <div>
+              <Image
+                src="/casalesolteiro.png"
+                width={300}
+                height={146}
+                alt="Picture of the author"
+              />
+              <h2>Quarto Família</h2>
+              <h2>R$500 diária</h2>
+            </div>
 
-      
-    
+            <div>
+              <Image
+                src="/premium.png"
+                width={300}
+                height={146}
+                alt="Picture of the author"
+              />
+              <h2>Quarto de luxo </h2>
+              <h2>R$500 diária</h2>
+            </div>
+            <div>
+              <Image
+                src="/doissolteiro.png"
+                width={300}
+                height={146}
+                alt="Picture of the author"
+              />
+              <h2>Quarto duplo solteiro</h2>
+              <h2>R$350 diária</h2>
+            </div>
+            <div>
+              <Image
+                src="/casalesolteiro.png"
+                width={300}
+                height={146}
+                alt="Picture of the author"
+              />
+              <h2>Quarto Família</h2>
+              <h2>R$500 diária</h2>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
