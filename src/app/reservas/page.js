228e1983ecb5d88fcb8dc12f99998b981f6ac157
milenaggoes <rm94305@fiap.com.br>
2023-09-04 15:32:32 -0300

    fetch

--- src/app/reservas/page.js
+++ src/app/reservas/page.js
@@ -1,15 +1,30 @@
 import NavBar from "@/components/NavBar";
+import DataRow from "./DataRow";
 
-export default function Reservas() {
+async function getReservas(){
+  const url = "http://localhost:8080/api/reservas"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  if (!resp.ok) throw new Error("Não pode carregar os dados")
+  return resp.json()
+}
+
+export default async function Home() {
+
+  const data = await getReservas();
+  
   return (
     <>
       <NavBar active={"reservas"}/>
-      <main>
-        <ul className="flex justify-center gap-20 mt-16 border-solid ">
-          <li className="bg-amber-500 w-44 rounded-xl shadow-md text-center">Todas</li>
-          <li className="w-44 rounded-xl shadow-md text-center">Proxímas</li>
-          <li className="w-44 rounded-xl shadow-md text-center">Finalizadas</li>
-        </ul> 
+      <main className="px-20 py-14">
+        <h1 className="text-center mb-16 ">Selecione um de nossos quartos disponíveis</h1>
+          <div id="data" className="flex justify-evenly flex-wrap ">
+          {data.map(()=>{
+            return(<DataRow reserva={reserva}/>)
+                
+            })}
+            
+          </div>
+        
       </main>
     </>
   )
